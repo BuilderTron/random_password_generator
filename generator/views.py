faa58d258e27c71c3f866da030123a0bafca91b7
Jose Lopez <jj.lopezbuilder@gmail.com>
2020-02-20 15:27:28 -0600

    Added a templates folder in generator app. Also created random password generator in views.py

--- generator/views.py
+++ generator/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import random
 
 # Create your views here.
 
 def home(request):
-    return HttpResponse("Hello there friend.")
+    return render(request, 'generator/home.html')
 
-def eggs(request):
-    return HttpResponse("Eggs are so tasty!")
+def password(request):
+
+    characters = list('abcdefghijklmnopqrstuvwxyz')
+    length = 10
+    thepassword = ''
+    for x in range(length):
+        thepassword += random.choice(characters)
+
+
+
+    return render(request, 'generator/password.html', {'password':thepassword})
