f8a20736ce2678de89872b2a39f499abb3f2b1b7
Jose <jj.lopezbuilder@gmail.com>
2020-04-21 11:04:34 -0500

    Added bootstrap, html, css, but is not activated yet.

--- password_generator/settings.py
+++ password_generator/settings.py
@@ -119,3 +119,22 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+
+#Media
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+#Local settings
+
+try:
+    from .local_settings import *
+except ImportError:
+    print("Looks like no local file. You must be in production.")
--- password_generator/urls.py
+++ password_generator/urls.py
@@ -16,10 +16,15 @@ Including another URLconf
 
 from django.urls import path
 from generator import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('',views.home, name= 'home'),
     path('about_us',views.about_us, name= 'about_us'),
     path('password',views.password, name= 'password'),
+    # path('index',views.index, name= 'index'),
 
 ]
+
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
